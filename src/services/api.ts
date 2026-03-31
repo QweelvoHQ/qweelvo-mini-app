e1e9053bd03bdc2eae3c05349ed0d5ee8ef9547a
Youssef Alaa <youssefalaacj@gmail.com>
2026-03-31 20:07:32 +0200

    feat: implement axios API service

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { store } from '@/app/store';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL + '/api/v1',
+  baseURL: 'http://qweelvo-order/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
